e7e4b1c65ae229c7e25b735d42166a367cb905a7
Chetan <chetanpatil9009@gmail.com>
2021-09-03 16:38:26 +0530

    Day 1

--- DomainLayer/GoalDomainEntity.cs
+++ DomainLayer/GoalDomainEntity.cs
@@ -6,7 +6,12 @@ namespace DomainLayer
 {
     class GoalDomainEntity
     {
-
+      
+        public int? CreatedBy { get; set; }
+        public string Title { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Score { get; set; }
 
     }
 }
